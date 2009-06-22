693c31e327e610c13494bf3c13602f783cf6b064
Marshall <mculpepper@appcelerator.org>
2009-06-22 14:25:25 -0500

    fix to titanium developer script for osx

--- build/titanium_developer.py
+++ build/titanium_developer.py
@@ -1,6 +1,6 @@
 # quick script to automate build/run of titanium developer
 import os, sys, platform, shutil, distutils.dir_util as dir_util
-import titanium_version
+import titanium_version, subprocess
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 os_map = { 'Windows': 'win32', 'Darwin': 'osx', 'Linux': 'linux' }
@@ -12,11 +12,14 @@ if os.path.exists(build_path):
 	dir_util.remove_tree(build_path)
 os.makedirs(build_path)
 
+def run(args):
+	return subprocess.Popen(args, stdout=subprocess.PIPE).communicate()[0]
+
 def get_sdk():
 	if os_name == 'win32':
 		return os.path.join('C:\\', 'ProgramData', 'Titanium')
 	elif os_name == 'osx':
-		return os.path.join('Library', 'Application Support', 'Titanium')
+		return os.path.join(os.path.expanduser('~'), 'Library', 'Application Support', 'Titanium')
 	elif os_name == 'linux':
 		# ??
 		return os.path.join('')
@@ -25,14 +28,4 @@ sdk_path = get_sdk()
 support_path = os.path.join(sdk_path, 'sdk', os_name, titanium_version.version)
 tibuild_path = os.path.join(support_path, 'tibuild.py')
 
-command = " %(script)s -d %(buildpath)s -s %(sdk)s %(project)s -a %(support)s -r" % {
-	'script': tibuild_path,
-	'buildpath': build_path,
-	'sdk': sdk_path,
-	'project': developer_path,
-	'support': support_path
-}
-
-command = sys.executable + command
-print command
-os.system(command)
+run([sys.executable, tibuild_path, '-d', build_path, '-s', sdk_path, developer_path, '-a', support_path, '-r'])
