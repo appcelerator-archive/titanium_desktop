f4fc58efcc1aea239457693622c1a1e23b3df7cc
Chris Warner <cWarner@appcelerator.com>
2009-06-29 14:51:52 -0700

    added more api covereage tests for Ti.Filesystem.FileStream

--- apps/drillbit/Resources/tests/api/api.js
+++ apps/drillbit/Resources/tests/api/api.js
@@ -3,6 +3,16 @@
 
 describe("ti.API tests",
 {
+
+    test_globals: function()
+    {
+        // function to testout the global properties in Titanium.
+        value_of(Titanium.platform).should_be_string();
+        value_of(Titanium.version).should_be_string();
+        
+        Titanium.API.info("Titanium.platform = "+Titanium.platform);
+        Titanium.API.info("Titanium.version = "+Titanium.version);
+    },
 	// test the logging functions
 	test_logging_methods: function()
 	{
