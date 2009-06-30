f44f72251dc463cf0fc98f2264fd39c60ee4de42
Chris Warner <cWarner@appcelerator.com>
2009-06-30 14:53:40 -0700

    added more coverage to Titanium.platform.  previous tests only checked for the existance of the methods, this version also checks some of the data returned from the methods.

--- apps/drillbit/Resources/tests/platform/platform.js
+++ apps/drillbit/Resources/tests/platform/platform.js
@@ -9,6 +9,10 @@ describe("ti.Platform tests", {
 			value_of(iface.name).should_be_string();
 			value_of(iface.address).should_be_string();
 			value_of(iface.displayName).should_be_string();
+			
+			Titanium.API.debug("interface "+ iface.name + 
+			                   ", " +  iface.address + 
+			                   ", " + iface.displayName );
 		}
 	},
 	
@@ -23,6 +27,36 @@ describe("ti.Platform tests", {
 		value_of(Titanium.Platform.processorCount).should_be_number();
 		value_of(Titanium.Platform.username).should_be_string();
 		value_of(Titanium.Platform.createUUID).should_be_function();
+
+		Titanium.API.debug("Titanium.Platform.name " +Titanium.Platform.name);
+		Titanium.API.debug("Titanium.Platform.ostype " +Titanium.Platform.ostype);
+		Titanium.API.debug("Titanium.Platform.version " +Titanium.Platform.version);
+		Titanium.API.debug("Titanium.Platform.architecture " +Titanium.Platform.architecture);
+		Titanium.API.debug("Titanium.Platform.address " +Titanium.Platform.address);
+		Titanium.API.debug("Titanium.Platform.id " +Titanium.Platform.id);
+		Titanium.API.debug("Titanium.Platform.macaddress " +Titanium.Platform.macaddress);
+		Titanium.API.debug("Titanium.Platform.processorCount " +Titanium.Platform.processorCount);
+		Titanium.API.debug("Titanium.Platform.username " +Titanium.Platform.username);
+
+        // TODO add more specific tests to insure we report the correct platform.
+        if ( Titanium.platform == "osx" )
+        {
+            value_of(Titanium.Platform.name).should_not_be("Windows NT");
+        }
+        else if ( Titanium.platform == "linux" )
+        {
+            value_of(Titanium.Platform.name).should_not_be("Windows NT");
+        }
+        else if ( Titanium.platform == "win32" )
+        {
+            value_of(Titanium.Platform.name).should_be("Windows NT");
+        }
+        else 
+        {
+            // this is a delibrate error.  we should never get here.
+            // but just in case we do, log it.
+            value_of(Titanium.Platform.name).should_not_be("unknown");
+        }
 	},
 	
 	test_unique_uuid: function() {
