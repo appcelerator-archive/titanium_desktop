855abf609142f2c38bbb0006ad99819bb0364101
Martin Robinson <mrobinson@appcelerator.com>
2009-06-30 13:43:13 -0700

    Update cross-site AJAX suite to include HTTPS test

--- apps/drillbit/Resources/tests/ajax_cross_domain/ajax_cross_domain.js
+++ apps/drillbit/Resources/tests/ajax_cross_domain/ajax_cross_domain.js
@@ -12,19 +12,54 @@ describe("ajax cross domain test",
 			success: function(data)
 			{
 				clearTimeout(timer);
-				try
+				if (typeof(data) !== 'object')
 				{
-					value_of(data).should_be_object();
-					callback.passed();
+					callback.failed("HTTP Twitter request did not return object");
 				}
-				catch(e)
+				else
 				{
-					callback.failed(e);
+					callback.passed();
 				}
 			},
 			error: function(xhr,status,error)
 			{
-				callback.failed(error);
+				var err = "HTTP Twitter request failed:";
+				err += (status | error | "Unknown");
+				callback.failed(err);
+			}
+		});
+
+		// fail test after 30s
+		timer = setTimeout(function()
+		{
+			callback.failed('ajax request timed out after 30s');
+		},30000)		
+	},
+	twitter_https_as_async:function(callback)
+	{
+		var timer = 0;
+		$.ajax({
+			type:'GET',
+			dataType:'json',
+			url:'https://twitter.com/statuses/public_timeline.json',
+
+			success: function(data)
+			{
+				clearTimeout(timer);
+				if (typeof(data) !== 'object')
+				{
+					callback.failed("HTTPS Twitter request did not return object");
+				}
+				else
+				{
+					callback.passed();
+				}
+			},
+			error: function(xhr,status,error)
+			{
+				var err = "HTTPS Twitter request failed:";
+				err += (status | error | "Unknown");
+				callback.failed(err);
 			}
 		});
 
