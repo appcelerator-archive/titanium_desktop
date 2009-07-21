16d92131ef2e53f95e777039ccbc342625b702bf
Marshall <mculpepper@appcelerator.org>
2009-07-21 17:23:13 -0500

    better errors when failing to create a pipe

--- modules/ti.Process/posix/posix_pipe.cpp
+++ modules/ti.Process/posix/posix_pipe.cpp
@@ -19,7 +19,9 @@ namespace ti
 			readHandle  = fds[0];
 			writeHandle = fds[1];
 		}
-		else throw ValueException::FromString("Error creating input pipe");
+		else {
+			throw ValueException::FromFormat("Error creating pipe: %s (%d)", strerror(errno), errno);
+		}
 	}
 
 	PosixPipe::~PosixPipe()
