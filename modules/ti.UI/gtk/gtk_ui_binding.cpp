fffa3bbf870dd3e3a8bb46ac04ba0ad972248045
Martin Robinson <mrobinson@appcelerator.com>
2009-07-27 11:55:43 -0700

    Enable proxy support for libsoup

--- modules/ti.UI/gtk/gtk_ui_binding.cpp
+++ modules/ti.UI/gtk/gtk_ui_binding.cpp
@@ -12,6 +12,8 @@
 #include <X11/extensions/scrnsaver.h>
 #include <gdk/gdkx.h>
 #include <Poco/Thread.h>
+#include <libsoup/soup.h>
+#include <libsoup/soup-gnome.h>
 
 namespace ti
 {
@@ -22,11 +24,15 @@ namespace ti
 		contextMenu(0),
 		iconPath("")
 	{
-		/* Prepare the custom URL handlers */
+		// Prepare the custom URL handlers
 		webkit_titanium_set_normalize_url_cb(NormalizeURLCallback);
 		webkit_titanium_set_url_to_file_url_cb(URLToFileURLCallback);
 
-		/* Register the script evaluator */
+		// Setup libsoup proxy support
+		SoupSession* session = webkit_get_default_session();
+		soup_session_add_feature_by_type(session, SOUP_TYPE_PROXY_RESOLVER_GNOME);
+
+		// Register the script evaluator
 		webkit_titanium_add_script_evaluator(evaluator);
 
 		char buf[256];
