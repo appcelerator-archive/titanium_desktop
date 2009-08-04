c907f484013f36d8dd66c4627b32011ca48f77b5
Marshall <mculpepper@appcelerator.org>
2009-08-04 18:12:26 -0500

    more name shortening madness, use "test"

--- apps/drillbit/Resources/tests/UI_Window/UI_Window.js
+++ apps/drillbit/Resources/tests/UI_Window/UI_Window.js
@@ -27,14 +27,14 @@ describe("UI Window Tests",{
 		};
 	},
 	
-	test_window_open_no_url: function()
+	test_open_no_url: function()
 	{
 		var w = window.open()
 		value_of(w).should_be_object();
 		value_of(w.Titanium).should_be_undefined();
 		w.close();
 	},
-	test_window_open_with_url_as_async: function(test)
+	test_open_with_url_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
@@ -42,7 +42,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium).should_be_object();
 		}, ["a.html"]);
 	},
-	test_window_open_with_name_as_async: function(test)
+	test_open_with_name_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
@@ -53,7 +53,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_false();
 		}, ["a.html", "a"]);
 	},
-	test_window_open_fullscreen_yes_as_async: function(test)
+	test_open_fullscreen_yes_as_async: function(test)
 	{
 		
 		this.async_window_open(test, function(w)
@@ -65,7 +65,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_true();
 		}, ["a.html","a","fullscreen=yes"]);
 	},
-	test_window_open_fullscreen_no_as_async: function(test)
+	test_open_fullscreen_no_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
@@ -76,7 +76,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_false();
 		}, ["a.html","a","fullscreen=no"]);
 	},
-	test_window_open_fullscreen_1_as_async: function(test)
+	test_open_fullscreen_1_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
@@ -87,7 +87,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_true();
 		}, ["a.html","a","fullscreen=1"]);
 	},
-	test_window_open_fullscreen_0_as_async: function(test)
+	test_open_fullscreen_0_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
@@ -98,7 +98,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_false();
 		}, ["a.html","a","fullscreen=0"]);
 	},
-	test_window_open_height_100_as_async: function(test)
+	test_open_height_100_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
@@ -109,7 +109,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium.UI.getCurrentWindow().getHeight()).should_be(100);
 		}, ["a.html","a","height=100"]);
 	},
-	test_window_open_width_100_as_async: function(test)
+	test_open_width_100_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
@@ -121,7 +121,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_true();
 		}, ["a.html","a","width=100"]);
 	},
-	test_window_open_resizable_1_as_async: function(test)
+	test_open_resizable_1_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
@@ -132,7 +132,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_true();
 		}, ["a.html","a","resizable=1"]);
 	},
-	test_window_open_resizable_0_as_async: function(test)
+	test_open_resizable_0_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
@@ -143,7 +143,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_false();
 		}, ["a.html","a","resizable=0"]);
 	},
-	test_window_open_resizable_true_as_async: function(test)
+	test_open_resizable_true_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
@@ -154,7 +154,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_true();
 		}, ["a.html","a","resizable=true"]);
 	},
-	test_window_open_resizable_false_as_async: function(test)
+	test_open_resizable_false_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
@@ -165,7 +165,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_false();
 		}, ["a.html","a","resizable=false"]);
 	},
-	test_window_open_left_as_async: function(test)
+	test_open_left_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
@@ -176,7 +176,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium.UI.getCurrentWindow().getX()).should_be(2);
 		}, ["a.html","a","left=2"]);
 	},
-	test_window_open_top_as_async: function(test)
+	test_open_top_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
@@ -187,7 +187,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium.UI.getCurrentWindow().getY()).should_be(2);
 		}, ["a.html","a","top=2"]);
 	},
-	test_window_open_multiple_props_as_async: function(test)
+	test_open_multiple_props_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
@@ -202,7 +202,7 @@ describe("UI Window Tests",{
 			value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_false();
 		}, ["a.html","a","left=2,top=2,fullscreen=0,width=10,height=10"]);
 	},
-	test_window_open_test_value_in_window_as_async: function(callback)
+	test_open_child_dom_as_async: function(test)
 	{
 		this.async_window_open(test, function(w)
 		{
