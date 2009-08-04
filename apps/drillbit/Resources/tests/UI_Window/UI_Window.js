172930526dd86af0be29cd7b65ff2bf5f07100df
Marshall <mculpepper@appcelerator.org>
2009-08-04 18:03:34 -0500

    use async in all UI_Window tests, still failing to get document.title

--- apps/drillbit/Resources/tests/UI_Window/UI_Window.js
+++ apps/drillbit/Resources/tests/UI_Window/UI_Window.js
@@ -1,4 +1,32 @@
 describe("UI Window Tests",{
+	before_all: function()
+	{
+		var self = this;
+		this.async_window_open = function(test, assertFn, args)
+		{
+			var w = window.open.apply(window, args);
+			value_of(w).should_be_object();
+			var timer = 0;
+			Titanium.API.addEventListener(Titanium.PAGE_INITIALIZED, function(event)
+			{
+				clearTimeout(timer);
+				try
+				{
+					assertFn.apply(self, [w]);
+					w.close();
+					test.passed();
+				}
+				catch (e)
+				{
+					test.failed(e);
+				}
+			});
+			timer = setTimeout(function(){
+				test.failed("open url timed out");
+			}, 3000);
+		};
+	},
+	
 	test_window_open_no_url: function()
 	{
 		var w = window.open()
@@ -6,177 +34,182 @@ describe("UI Window Tests",{
 		value_of(w.Titanium).should_be_undefined();
 		w.close();
 	},
-	test_window_open_with_url: function()
+	test_window_open_with_url_as_async: function(test)
 	{
-		var w = window.open("a.html")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		w.close();
+		this.async_window_open(test, function(w)
+		{
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium).should_be_object();
+		}, ["a.html"]);
 	},
-	test_window_open_with_url_with_name: function()
+	test_window_open_with_name_as_async: function(test)
 	{
-		var w = window.open("a.html","a")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
-		value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_false();
-		w.close();
+		this.async_window_open(test, function(w)
+		{
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
+			value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_false();
+		}, ["a.html", "a"]);
 	},
-	test_window_open_with_url_with_fullscreen_as_string_true: function()
+	test_window_open_fullscreen_yes_as_async: function(test)
 	{
-		var w = window.open("a.html","a","fullscreen=yes")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
-		value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_true();
-		w.close();
+		
+		this.async_window_open(test, function(w)
+		{
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
+			value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_true();
+		}, ["a.html","a","fullscreen=yes"]);
 	},
-	test_window_open_with_url_with_fullscreen_as_string_false: function()
+	test_window_open_fullscreen_no_as_async: function(test)
 	{
-		var w = window.open("a.html","a","fullscreen=no")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
-		value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_false();
-		w.close();
+		this.async_window_open(test, function(w)
+		{
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
+			value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_false();
+		}, ["a.html","a","fullscreen=no"]);
 	},
-	test_window_open_with_url_with_fullscreen_as_number: function()
+	test_window_open_fullscreen_1_as_async: function(test)
 	{
-		var w = window.open("a.html","a","fullscreen=1")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
-		value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_true();
-		w.close();
+		this.async_window_open(test, function(w)
+		{
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
+			value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_true();
+		}, ["a.html","a","fullscreen=1"]);
 	},
-	test_window_open_with_url_with_fullscreen_as_number_zero: function()
+	test_window_open_fullscreen_0_as_async: function(test)
 	{
-		var w = window.open("a.html","a","fullscreen=0")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
-		value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_false();
-		w.close();
+		this.async_window_open(test, function(w)
+		{
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
+			value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_false();
+		}, ["a.html","a","fullscreen=0"]);
 	},
-	test_window_open_with_url_with_height: function()
+	test_window_open_height_100_as_async: function(test)
 	{
-		var w = window.open("a.html","a","height=100")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
-		value_of(w.Titanium.UI.getCurrentWindow().getHeight()).should_be(100);
-		w.close();
+		this.async_window_open(test, function(w)
+		{
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
+			value_of(w.Titanium.UI.getCurrentWindow().getHeight()).should_be(100);
+		}, ["a.html","a","height=100"]);
 	},
-	test_window_open_with_url_with_width: function()
+	test_window_open_width_100_as_async: function(test)
 	{
-		var w = window.open("a.html","a","width=100")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
-		value_of(w.Titanium.UI.getCurrentWindow().getWidth()).should_be(100);
-		value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_true();
-		w.close();
+		this.async_window_open(test, function(w)
+		{
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
+			value_of(w.Titanium.UI.getCurrentWindow().getWidth()).should_be(100);
+			value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_true();
+		}, ["a.html","a","width=100"]);
 	},
-	test_window_open_with_url_with_resizable_as_number: function()
+	test_window_open_resizable_1_as_async: function(test)
 	{
-		var w = window.open("a.html","a","resizable=1")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
-		value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_true();
-		w.close();
+		this.async_window_open(test, function(w)
+		{
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
+			value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_true();
+		}, ["a.html","a","resizable=1"]);
 	},
-	test_window_open_with_url_with_resizable_as_number_zero: function()
+	test_window_open_resizable_0_as_async: function(test)
 	{
-		var w = window.open("a.html","a","resizable=0")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
-		value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_false();
-		w.close();
+		this.async_window_open(test, function(w)
+		{
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
+			value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_false();
+		}, ["a.html","a","resizable=0"]);
 	},
-	test_window_open_with_url_with_resizable_as_boolean: function()
+	test_window_open_resizable_true_as_async: function(test)
 	{
-		var w = window.open("a.html","a","resizable=true")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
-		value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_true();
-		w.close();
+		this.async_window_open(test, function(w)
+		{
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
+			value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_true();
+		}, ["a.html","a","resizable=true"]);
 	},
-	test_window_open_with_url_with_resizable_as_boolean_false: function()
+	test_window_open_resizable_false_as_async: function(test)
 	{
-		var w = window.open("a.html","a","resizable=false")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
-		value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_false();
-		w.close();
+		this.async_window_open(test, function(w)
+		{
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
+			value_of(w.Titanium.UI.getCurrentWindow().isResizable()).should_be_false();
+		}, ["a.html","a","resizable=false"]);
 	},
-	test_window_open_with_url_with_left: function()
+	test_window_open_left_as_async: function(test)
 	{
-		var w = window.open("a.html","a","left=2")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
-		value_of(w.Titanium.UI.getCurrentWindow().getX()).should_be(2);
-		w.close();
+		this.async_window_open(test, function(w)
+		{
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
+			value_of(w.Titanium.UI.getCurrentWindow().getX()).should_be(2);
+		}, ["a.html","a","left=2"]);
 	},
-	test_window_open_with_url_with_top: function()
+	test_window_open_top_as_async: function(test)
 	{
-		var w = window.open("a.html","a","top=2")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
-		value_of(w.Titanium.UI.getCurrentWindow().getY()).should_be(2);
-		w.close();
+		this.async_window_open(test, function(w)
+		{
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
+			value_of(w.Titanium.UI.getCurrentWindow().getY()).should_be(2);
+		}, ["a.html","a","top=2"]);
 	},
-	test_window_open_with_url_with_multiple_props: function()
+	test_window_open_multiple_props_as_async: function(test)
 	{
-		var w = window.open("a.html","a","left=2,top=2,fullscreen=0,width=10,height=10")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
-		value_of(w.Titanium.UI.getCurrentWindow().getY()).should_be(2);
-		value_of(w.Titanium.UI.getCurrentWindow().getX()).should_be(2);
-		value_of(w.Titanium.UI.getCurrentWindow().getWidth()).should_be(10);
-		value_of(w.Titanium.UI.getCurrentWindow().getHeight()).should_be(10);
-		value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_false();
-		w.close();
+		this.async_window_open(test, function(w)
+		{
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.Titanium.UI.getCurrentWindow()).should_be_object();
+			value_of(w.Titanium.UI.getCurrentWindow().getY()).should_be(2);
+			value_of(w.Titanium.UI.getCurrentWindow().getX()).should_be(2);
+			value_of(w.Titanium.UI.getCurrentWindow().getWidth()).should_be(10);
+			value_of(w.Titanium.UI.getCurrentWindow().getHeight()).should_be(10);
+			value_of(w.Titanium.UI.getCurrentWindow().isFullscreen()).should_be_false();
+		}, ["a.html","a","left=2,top=2,fullscreen=0,width=10,height=10"]);
 	},
 	test_window_open_test_value_in_window_as_async: function(callback)
 	{
-		var w = window.open("a.html")
-		value_of(w).should_be_object();
-		value_of(w.Titanium).should_be_object();
-		value_of(w.document.title).should_be("Hello");
-		setTimeout(function()
+		this.async_window_open(test, function(w)
 		{
-			try
-			{
-				value_of(w.result).should_be('Hello');
-				callback.passed();
-			}
-			catch(e)
-			{
-				callback.failed(e);
-			}
-			w.close();
-		},1000);
+			value_of(w).should_be_object();
+			value_of(w.Titanium).should_be_object();
+			value_of(w.document.title).should_be("Hello");
+			value_of(w.result).should_be('Hello');
+		}, ["a.html"]);
 	},
 });
