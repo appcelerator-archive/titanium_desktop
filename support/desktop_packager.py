a2d5e47773e3ab823c1f4b68b44ee2e79d892510
jhaynie <jhaynie@gmail.com>
2009-08-13 19:59:54 -0700

    [#95 state:resolved] fixed problem with OSX Universal build. Need to test on PPC but confirmed in Info Window on Finder [#96 state:resolved] fixed problem where the DMG background for OSX not being properly used, icon positioning, etc

--- support/desktop_packager.py
+++ support/desktop_packager.py
@@ -30,7 +30,6 @@ class DesktopPackager(object):
 
 	def create_zip(self, builder):
 		extractor = os.path.join(self.options.assets_dir, 'self_extractor.exe')
-		#exe = os.path.join(self.options.destination,builder.options.executable)
 		exe = builder.options.executable
 		shutil.copy(extractor,exe)
 		builder.log("making win32 binary at %s, this will take a sec..." % exe)
@@ -73,6 +72,16 @@ class DesktopPackager(object):
 		tar.close()
 		return outtarfile
 
+	def folder_size_in_MB(self,folder,add_to_it=0):
+		folder_size = add_to_it
+		for (path, dirs, files) in os.walk(folder):
+		  for file in files:
+		    filename = os.path.join(path, file)
+		    folder_size += os.path.getsize(filename)
+
+		return folder_size/(1024*1024.0)
+
+
 	def create_dmg(self,builder):	
 
 		dmg = os.path.join(self.options.destination, builder.appname)
@@ -105,14 +114,22 @@ class DesktopPackager(object):
 		self.log("DMG Source: %s" % builder.base_dir)
 		self.log("Temp DMG: %s.dmg" % temp_dmg)
 		self.log("Building: %s.dmg" % dmg)
+		
+		# get the size of the image
+		add_to_size = 0
+		if self.options.dmg_background: add_to_size = os.path.getsize(self.builder.dmg_background)
+		disk_size_in_MB = 1 + self.folder_size_in_MB(builder.base_dir,add_to_size)
 
 		# now run the DMG packager
-		builder.invoke("hdiutil create -srcfolder \"%s\" -scrub -volname \"%s\" -fs HFS+ -fsargs \"-c c=64,a=16,e=16\" -format UDRW \"%s\"" % (builder.base_dir,builder.appname,temp_dmg))
-		builder.invoke("hdiutil attach -mountpoint \"%s\" -noverify -noautofsck -nobrowse -readwrite -noverify -noautoopen \"%s.dmg\"" % (volname,temp_dmg))
+		builder.invoke("hdiutil create -srcfolder \"%s\" -scrub -volname \"%s\" -fs HFS+ -fsargs \"-c c=64,a=16,e=16\" -megabytes %d -format UDRW \"%s\"" % (builder.base_dir,builder.appname,disk_size_in_MB,temp_dmg))
+ 		builder.invoke("hdiutil attach -mountpoint \"%s\" -noverify -noautofsck -nobrowse -readwrite -noverify -noautoopen \"%s.dmg\"" % (volname,temp_dmg))
 		builder.invoke("bless --folder \"%s\" --openfolder \"%s\"" % (volname,volname))
 		time.sleep(1) # give it a chance to update
 
 		builder.invoke("ln -s /Applications \"%s/Applications\"" % (volname))
+		
+		ds_store = os.path.join(self.options.assets_dir, 'ds_store')
+		shutil.copy(ds_store,os.path.join(volname,".DS_Store"))
 
 		imagedir = os.path.join(volname,'.background')
 		os.makedirs(imagedir)
@@ -124,55 +141,13 @@ class DesktopPackager(object):
 			dmg_bg_ext = '.jpg'
 			builder.invoke("ditto \"%s/background.jpg\" \"%s\"" % (self.options.assets_dir,imagedir))
 
-		OSA = """
-tell application "Finder"
-	set f to POSIX file ("%s" as string) as alias
-	tell folder f
-		open
-		tell container window
-			set toolbar visible to false
-			set statusbar visible to false
-			set current view to icon view
-			delay 1 -- Sync
-			set the bounds to {300, 200, 1000, 1000}
-		end tell
-		delay 1 -- Sync
-		set icon size of the icon view options of container window to 128
-		set arrangement of the icon view options of container window to not arranged
-		set background picture of the icon view options of container window to file ".background:background%s"
-		set position of item "%s.app" to {120, 180}
-		set position of item "Applications" to {350, 180}
-		set the bounds of the container window to {50, 100, 520, 550}
-		set shows item info of the icon view options of container window to false
-		set position of the container window to {395, 190}
-		update without registering applications
-		delay 5 -- Sync
-		close
-	end tell
-	-- Sync
-	delay 5
-end tell
-""" % (os.path.abspath(volname), dmg_bg_ext, self.appname)
-		
-		
-		osa = os.path.join(self.options.destination,'osa')
-		osafile = open(osa,'w+')
-		osafile.write(OSA)
-		osafile.close()
-		self.log("Executing finder configuration ... one moment")
-		self.log("OSAscript: \n%s" % OSA)
-		builder.invoke("osascript \"%s\"" % osa)
-
 		builder.invoke("ditto \"%s\" \"%s/.VolumeIcon.icns\"" % (app_icns,volname))
 		builder.invoke("/Developer/Tools/SetFile -a C \"%s\"" % volname)
 		builder.invoke("/Developer/Tools/SetFile -a V \"%s/.background/background.jpg\"" % volname)
-
-		bin = os.path.join(volname,"%s.app" % self.appname,"Contents","MacOS",self.appname)
-		builder.invoke("lipo -thin i386 \"%s\" -output \"%s\"" % (bin,bin))
 		
 		builder.invoke("hdiutil detach \"%s\"" % volname)
 		builder.invoke("hdiutil convert \"%s.dmg\" -format UDBZ -imagekey zlib-level=9 -o \"%s.dmg\"" % (temp_dmg,dmg))
-		os.remove(temp_dmg+'.dmg')
+ 		os.remove(temp_dmg+'.dmg')
 
 		self.log("osx packager created: \"%s.dmg\"" % dmg)
 
