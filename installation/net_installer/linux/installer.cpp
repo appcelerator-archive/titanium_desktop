5cf1af798a43b381553f8f6a1687047795b04ff0
Martin Robinson <martin@jones.(none)>
2009-09-18 17:12:44 -0700

    Don't query the combo box in the Linux installer when there isn't one.

--- installation/net_installer/linux/installer.cpp
+++ installation/net_installer/linux/installer.cpp
@@ -479,11 +479,13 @@ GtkWidget* Installer::GetApplicationIcon()
 
 void Installer::StartInstallProcess()
 {
-	int choice = gtk_combo_box_get_active(GTK_COMBO_BOX(this->installCombo));
-	if (choice == 0)
-		this->installType = HOMEDIR_INSTALL;
-	else
-		this->installType = SYSTEM_INSTALL;
+	this->installType = HOMEDIR_INSTALL;
+	if (this->installCombo)
+	{
+		int choice = gtk_combo_box_get_active(GTK_COMBO_BOX(this->installCombo));
+		if (choice != 0)
+			this->installType = SYSTEM_INSTALL;
+	}
 
 	// Home directory install or we already have a root-level effective userid
 	if (this->installType == HOMEDIR_INSTALL || geteuid() == 0)
