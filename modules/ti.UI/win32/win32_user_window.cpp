b4478e84aa5b280a032f5c8ab50260e41655c78d
Marshall Culpepper <mculpepper@appcelerator.com>
2009-11-15 01:53:02 -0600

    use PRODUCT_VERSION when setting UserAgent in windows (fixes UserAgent test)

--- modules/ti.UI/win32/win32_user_window.cpp
+++ modules/ti.UI/win32/win32_user_window.cpp
@@ -236,11 +236,11 @@ void Win32UserWindow::InitWebKit()
 		HandleHResultError("Error creating WebKitWebView", hr, true);
 
 	// Set the custom user agent for Titanium
-	const char *version = host->GetGlobalObject()->Get("version")->ToString();
+	//const char *version = host->GetGlobalObject()->Get("version")->ToString();
 	char userAgent[128];
 	//TI-303 we need to add safari UA to our UA to resolve broken
 	//sites that look at Safari and not WebKit for UA
-	sprintf(userAgent, "Version/4.0 Safari/528.16 %s/%s", PRODUCT_NAME, version);
+	sprintf(userAgent, "Version/4.0 Safari/528.16 %s/%s", PRODUCT_NAME, PRODUCT_VERSION);
 	_bstr_t ua(userAgent);
 	webView->setApplicationNameForUserAgent(ua.copy());
 
