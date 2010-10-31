95143e343d44761e42888ddf6147c1e21aaa21aa
Josh Roesslein <jroesslein@appcelerator.com>
2010-10-30 21:29:08 -0500

    More app module tests.

--- tests/modules/app/app.js
+++ tests/modules/app/app.js
@@ -38,3 +38,59 @@ describe("getDescription", function () {
         expect(Titanium.App.getDescription()).toEqual("A testing harness for Titanium");
     });
 });
+
+describe("getGUID", function () {
+    it("returns the application's GUID", function () {
+        expect(Titanium.App.getGUID()).toEqual("5FDFCB8A-CD45-423F-BF0C-01B025A07D34");
+    });
+});
+
+describe("getHome", function () {
+    var appHomeDir;
+
+    beforeEach(function () {
+        appHomeDir = Titanium.Filesystem.getFile(Titanium.App.getHome());
+    });
+
+    it("provides the application home directory", function () {
+        expect(appHomeDir.exists()).toBe(true);
+    });
+
+    it("should contain the resources folder", function () {
+        expect(appHomeDir.resolve("Resources").exists()).toBe(true);
+    });
+});
+
+// TODO: implement test for getIcon()
+xdescribe("getIcon", function () {});
+
+describe("getName", function () {
+    it("returns the name of the application", function () {
+        expect(Titanium.App.getName()).toEqual("Drillbit");
+    });
+});
+
+describe("getPath", function () {
+    it("returns an absolute path to the application's executable", function () {
+        var appPath = Titanium.App.getPath();
+        expect(Titanium.Filesystem.getFile(appPath).exists()).toBe(true);
+    });
+});
+
+describe("getPublisher", function () {
+    it("returns the name of the publisher of this application", function () {
+        expect(Titanium.App.getPublisher()).toEqual("Appcelerator");
+    });
+});
+
+describe("getApplication", function () {
+    it("returns the URL of this application", function () {
+        expect(Titanium.App.getURL()).toEqual("appcelerator.com");
+    });
+});
+
+describe("getVersion", function () {
+    it("returns the version of the application", function () {
+        expect(Titanium.App.getVersion()).toEqual("1.0");
+    });
+});
