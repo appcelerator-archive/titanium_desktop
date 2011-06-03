f5a94ec9b9df3afba6445cdf454544f39344a51e
Josh Roesslein <jroesslein@appcelerator.com>
2011-06-03 13:56:31 -0500

    [TIDESK-577] Correctly set contents of child windows.
    A mistake was made in the past that was setting the title
    instead of the contents in the window configuration.

--- modules/ti.App/WindowConfig.cpp
+++ modules/ti.App/WindowConfig.cpp
@@ -149,7 +149,7 @@ AutoPtr<WindowConfig> WindowConfig::FromProperties(KObjectRef properties)
     c->SetURL(properties->GetString("url", c->GetURL()));
     c->SetURLRegex(properties->GetString("urlRegex", c->GetURLRegex()));
     c->SetTitle(properties->GetString("title", c->GetTitle()));
-    c->SetTitle(properties->GetString("contents", c->GetContents()));
+    c->SetContents(properties->GetString("contents", c->GetContents()));
     c->SetURLRegex(properties->GetString("baseURL", c->GetBaseURL()));
     c->SetX(properties->GetInt("x", c->GetX()));
     c->SetY(properties->GetInt("y", c->GetY()));
